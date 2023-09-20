357f123ba21bb845f5f3f4d974bacab259bc0b2f
misterrodg <65290193+misterrodg@users.noreply.github.com>
2023-09-20 10:24:55 -0400

    Initial Argument Handling

--- draw.py
+++ draw.py
@@ -1,5 +1,25 @@
-def main():
+import argparse
+
+
+def processFacility():
     pass
+
+
+def main():
+    # Set up Defaults
+    # Set up Argument Handling
+    parser = argparse.ArgumentParser(description="VidMapper")
+    parser.add_argument(
+        "--facility", type=str, help="The three letter identifier for the facility."
+    )
+    args = parser.parse_args()
+    facility = args.facility
+    if facility:
+        processFacility(args.facility)
+    else:
+        print("Please select a facility with the facility flag.")
+        print("Example: --facility=AAA")
+
 
 if __name__ == "__main__":
     main()
