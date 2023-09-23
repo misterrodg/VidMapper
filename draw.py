847fbae86645e9f65c2e27bf9a89d3514b21bd8e
misterrodg <65290193+misterrodg@users.noreply.github.com>
2023-09-23 00:37:24 -0400

    Add Purge Command Line Argument

--- draw.py
+++ draw.py
@@ -1,6 +1,14 @@
 from modules.Facility import Facility
+from modules.FileHandler import FileHandler
 
 import argparse
+
+
+def purgeNavData():
+    fh = FileHandler()
+    dirs = ["airports", "fixes", "restrictive", "vors"]
+    for dir in dirs:
+        fh.deleteAllInSubdir(".json", f"./navdata/{dir}")
 
 
 def processFacility(id):
@@ -14,8 +22,12 @@ def main():
     parser.add_argument(
         "--facility", type=str, help="The three letter identifier for the facility."
     )
+    parser.add_argument("--purge", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
+    purge = args.purge
     facility = args.facility
+    if purge:
+        purgeNavData()
     if facility:
         processFacility(args.facility)
     else:
